a30d279bf5f022501b0471149f7e3f07facddca2
Mba96 <mbajameel90@gmail.com>
2022-12-26 14:37:27 +0000

    Developed the Race Day Project

--- day8.js
+++ day8.js
@@ -65,3 +65,62 @@ switch (eightBall) {
 //Creation of log for eightBall answer
 console.log(eightBall);
 
+
+
+/*
+Race Day
+Codecademy’s annual race is just around the corner! This year, we have a lot of participants. You have been hired to write a program that will register runners for the race and give them instructions on race day.
+
+As a timeline, registration would look like this:
+
+
+
+Here’s how our registration works. There are adult runners (over 18 years of age) and youth runners (under 18 years of age). They can register early or late. Runners are assigned a race number and start time based on their age and registration.
+
+Race number:
+
+Early adults receive a race number at or above 1000.
+All others receive a number below 1000.
+Start time:
+
+Adult registrants run at 9:30 am or 11:00 am.
+Early adults run at 9:30 am.
+Late adults run at 11:00 am.
+Youth registrants run at 12:30 pm (regardless of registration).
+But we didn’t plan for runners that are exactly 18! We’ll handle that by the end of the project.
+
+*/
+
+
+
+
+
+
+let raceNumber = Math.floor(Math.random() * 1000);
+
+//creation of variable to determine time of registration
+const registeredEarly = false;
+
+//creation of variable to determine runners age
+const age = 18;
+
+//control flow statement to check if runner is an adult AND registered early
+if (age > 18 && registeredEarly)
+raceNumber += 1000;
+
+//create statement to determine race time
+if (age > 18 && registeredEarly) {
+  console.log(`${raceNumber}, Race time is 9:30 am`);
+} else if (age > 18 && !registeredEarly){
+  console.log(`${raceNumber}, race time is 11:00 am `);
+} else if (age < 18 ){
+  console.log(`${raceNumber}, race time is 12:30 pm`);
+} else {
+  console.log('Runner to see the registration desk.');
+}
+
+
+
+
+
+
