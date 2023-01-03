fb79c4195385f1ec570dd942be3d7b49e1e20670
Mba96 <mbajameel90@gmail.com>
2023-01-03 23:07:40 +0000

    JS Iterators

--- day15.js
+++ day15.js
@@ -45,4 +45,57 @@ Utamaro is one of my favorite artists.
 [ 5, 3.14, 100 ]
 
 
+
+
+
+
+
+
+THE .FOREACH() METHOD:
+The first iteration method that we’re going to learn is .forEach(). Aptly named, .forEach() will execute the same code for each element of an array.
+
+
+Iterate over the fruits array to log I want to eat a plus the name of each fruit to the console. For example, I want to eat a mango.
 */
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(fruit => console.log(`I want to eat a ${fruit}.`))
+
+/*
+OUTPUT:
+
+Output:
+I want to eat a mango.
+I want to eat a papaya.
+I want to eat a pineapple.
+I want to eat a apple.
+
+*/
+
+/*
+THE .MAP() METHOD:
+When .map() is called on an array, it takes an argument of a callback function and returns a new array! Take a look at an example of calling .map():
+
+
+*/
+const numbers = [1, 2, 3, 4, 5]; 
+ 
+const bigNumbers = numbers.map(number => {
+  return number * 10;
+});
+
+/*
+.map() works in a similar manner to .forEach()— the major difference is that .map() returns a new array.
+
+In the example above:
+
+numbers is an array of numbers.
+bigNumbers will store the return value of calling .map() on numbers.
+numbers.map will iterate through each element in the numbers array and pass the element into the callback function.
+return number * 10 is the code we wish to execute upon each element in the array. This will save each value from the numbers array, multiplied by 10, to a new array.
+
+
+
+*/
+
