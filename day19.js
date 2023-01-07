b3c2d8c95c7a25ee48b081a63f35a49211df2f8e
Mba96 <mbajameel90@gmail.com>
2023-01-07 23:57:14 +0000

    JS Nested Objects

--- day19.js
+++ day19.js
@@ -36,3 +36,109 @@ const spaceship = {
         }
    }
 }; 
+
+/*
+We can chain operators to access nested properties. We’ll have to pay attention to which operator makes sense to use in each layer.
+ It can be helpful to pretend you are the computer and evaluate each expression from left to right so that each operation starts to feel a little more manageable.
+
+
+*/
+
+spaceship.nanoelectronics['back-up'].battery; // Returns 'Lithium'
+
+
+
+
+
+//EXERCISE:
+let spaceship = {
+    passengers: [{name:'Space Ape'}],
+    telescope: {
+      yearBuilt: 2018,
+      model: "91031-XLT",
+      focalLength: 2032 
+    },
+    crew: {
+      captain: { 
+        name: 'Sandra', 
+        degree: 'Computer Engineering', 
+        encourageTeam() { console.log('We got this!') },
+       'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+    },
+    engine: {
+      model: "Nimbus2000"
+    },
+    nanoelectronics: {
+      computer: {
+        terabytes: 100,
+        monitors: "HD"
+      },
+      'back-up': {
+        battery: "Lithium",
+        terabytes: 50
+      }
+    }
+  }; 
+  
+  let capFave = spaceship.crew.captain['favorite foods'][0];
+  
+  let firstPassenger = spaceship.passengers[0];
+
+
+
+
+  //PASS BY REFERENCE:
+  /*
+Objects are passed by reference. 
+This means when we pass a variable assigned to an object into a function as an argument, the computer interprets the parameter name as pointing to the space in memory holding that object. 
+As a result, functions which change object properties actually mutate the object permanently (even when the object is assigned to a const variable).
+
+  */
+
+
+
+  const spaceship = {
+    homePlanet : 'Earth',
+    color : 'silver'
+  };
+   
+  let paintIt = obj => {
+    obj.color = 'glorious gold'
+  };
+   
+  paintIt(spaceship);
+   
+  spaceship.color // Returns 'glorious gold'
+
+
+
+  
+
+  /*
+Our function paintIt() permanently changed the color of our spaceship object. 
+However, reassignment of the spaceship variable wouldn’t work in the same way:
+
+  */
+
+let spaceship = {
+    homePlanet : 'Earth',
+    color : 'red'
+  };
+  let tryReassignment = obj => {
+    obj = {
+      identified : false, 
+      'transport type' : 'flying'
+    }
+    console.log(obj) // Prints {'identified': false, 'transport type': 'flying'}
+   
+  };
+  tryReassignment(spaceship) // The attempt at reassignment does not work.
+  spaceship // Still returns {homePlanet : 'Earth', color : 'red'};
+   
+  spaceship = {
+    identified : false, 
+    'transport type': 'flying'
+  }; // Regular reassignment still works.
+
+  
+  
